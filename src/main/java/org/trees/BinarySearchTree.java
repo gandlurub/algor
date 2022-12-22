df0511114e1f2484887c7108f9c09f3732df4b8e
Bharath Gandluru <srisikharam@gmail.com>
2022-12-21 22:31:36 -0600

    adding oredecessor and successor

--- src/main/java/org/trees/BinarySearchTree.java
+++ src/main/java/org/trees/BinarySearchTree.java
@@ -3,6 +3,7 @@ package org.trees;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.zip.CheckedInputStream;
 
 public class BinarySearchTree {
 
@@ -37,8 +38,14 @@ public class BinarySearchTree {
         if(node != null)
             System.out.println("min value found :: "+node.value);
 
-        node = delete(root,32);
+        node = delete(root,76);
         System.out.println("Node deleted");
+
+        node = successor(root,80);
+        System.out.println("Sucessor found at location :: "+node.value);
+
+        node = predecessor(root,65);
+        System.out.println("Predecessor found at location :: "+node.value);
 
     }
 
@@ -154,6 +161,10 @@ public class BinarySearchTree {
 
           }
 
+          if(current == null){
+              return root;
+          }
+
          /* if node is a child node */
           if(current.left == null && current.right == null){
               if(prev.value < value){
@@ -165,21 +176,105 @@ public class BinarySearchTree {
 
           /* if a node has one child node */
            Node child = null;
-          if(current.left == null && current.right != null) {
-                child = current.right;
-          } else if(current.left != null && current.right == null){
-              child = current.left;
-          }
 
-          if(prev.left == current){
-              prev.left = child;
-          } else if(prev.right == current){
-              prev.right = child;
-          }
+          if(current.left == null || current.right == null) {
+              if (current.left == null && current.right != null) {
+                  child = current.right;
+              } else if (current.left != null && current.right == null) {
+                  child = current.left;
+              }
 
+              if (prev.left == current) {
+                  prev.left = child;
+              } else if (prev.right == current) {
+                  prev.right = child;
+              }
+          }
+        /* if a child had two nodes */
+
+           if(current.left != null && current.right != null){
+               Node successor= current.right;
+               while(successor.left != null){
+                     prev = successor;
+                     successor = successor.left;
+               }
+               current.value = successor.value;
+
+               if(successor == prev.left){
+                   prev.left = successor.right;
+               } else {
+                   prev.right = successor.right;
+               }
+
+           }
 
 
           return root;
+       }
+
+       static Node successor(Node root,int value){
+        if(root == null){
+            return root;
+        }
+
+        Node current = root;
+        Node ancestor = null;
+        while(current != null){
+            if(current.value == value){
+                break;
+            } else if(current.value > value){
+                ancestor = current;
+                current = current.left;
+            } else if (current.value < value) {
+
+                current = current.right;
+            }
+        }
+
+        if(current.right != null) {
+            Node child = current.right;
+            Node pre = null;
+            while (child != null) {
+                pre=child;
+                child = child.left;
+            }
+            return pre;
+        }
+
+
+        return ancestor;
+       }
+
+       static Node predecessor(Node root,int value){
+        if(root == null){
+            return null;
+        }
+
+        Node current = root;
+        Node prev = null;
+
+        while(current != null){
+            if(current.value == value) {
+                break;
+            } else if(current.value < value) {
+                prev = current;
+                current = current.right;
+            } else if(current.value > value) {
+                current = current.left;
+            }
+
+            }
+
+        if(current.left != null) {
+            Node child = current.left;
+            while (child != null) {
+                prev = child;
+                child = child.right;
+            }
+        }
+
+        return prev;
+
        }
 
 }
