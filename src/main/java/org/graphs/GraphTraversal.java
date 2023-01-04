d83c52f69d91457194e9a2c7bdf6b9b99db80f38
Bharath Gandluru <srisikharam@gmail.com>
2023-01-04 14:00:28 -0600

    Adding git traversal

--- src/main/java/org/graphs/GraphTraversal.java
+++ src/main/java/org/graphs/GraphTraversal.java
@@ -2,12 +2,15 @@ package org.graphs;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class GraphTraversal {
 
 
     int[] visited;
-    public boolean hasEulerianCycle(LinkedList<Integer>[] adjList){
+    int[] parent;
+    LinkedList<Integer>[] adjList;
+    public boolean hasEulerianCycle(LinkedList<Integer>[] adjList) {
 
         int odd = 0;
         for (LinkedList<Integer> integers : adjList) {
@@ -18,65 +21,118 @@ public class GraphTraversal {
         return odd == 0 || odd == 2;
     }
 
-    public int hasComponents(LinkedList<Integer>[] adjList){
+    public int hasComponents(LinkedList<Integer>[] adjList) {
         this.visited = new int[adjList.length];
 
-            bfs(0,adjList);
+        bfs(0, adjList);
 
-            /* Code to find out components based on visited */
-            int count = 0;
-            for(int i=0;i<visited.length;i++){
-                if(visited[i] == 0){
-                    bfs(i,adjList);
-                    count++;
-                }
+        /* Code to find out components based on visited */
+        int count = 0;
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i] == 0) {
+                bfs(i, adjList);
+                count++;
             }
+        }
 
         return count;
     }
 
 
-    public void bfs(int source,LinkedList<Integer>[] adjList){
+    public void bfs(int source, LinkedList<Integer>[] adjList) {
 
         Queue<Integer> queue = new LinkedList<>();
-                        queue.add(source);
+        queue.add(source);
+        visited[source] = 1;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int j = 0; j < adjList[current].size(); j++) {
+                int vertex = adjList[current].get(j);
+                if (visited[vertex] == 0) {
+                    queue.add(vertex);
+                    visited[vertex] = 1;
+                }
+            }
+        }
+    }
+
+    public boolean isValidTree_bfs_approach(int source, LinkedList<Integer>[] adjList) {
+        visited = new int[adjList.length];
+        parent = new int[adjList.length];
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+        visited[source] = 1;
+        while (!queue.isEmpty()) {
+            int vertex = queue.poll();
+
+            for (int j = 0; j < adjList[vertex].size(); j++) {
+                int current = adjList[vertex].get(j);
+                if (visited[current] == 0) {
+                    queue.add(current);
+                    visited[current] = 1;
+                    parent[current] = vertex;
+                } else {
+                    if (parent[vertex] != current) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isValidTree_dfs_stack_approach(int source,LinkedList<Integer>[] adjList){
+         visited = new int[adjList.length];
+         parent = new int[adjList.length];
+
+         Stack<Integer> stack = new Stack<>();
+                        stack.add(source);
                         visited[source] = 1;
 
-             while(!queue.isEmpty()){
-                 int current = queue.poll();
-                 for(int j=0;j<adjList[current].size();j++){
-                     int vertex = adjList[current].get(j);
-                     if(visited[vertex] == 0){
-                         queue.add(vertex);
-                         visited[vertex] = 1;
-                     }
-                 }
-             }
+                while(!stack.isEmpty()) {
+                   int vertex = stack.pop();
+                   for(int i=0;i<adjList[vertex].size();i++){
+                       int current = adjList[vertex].get(i);
+                        if(visited[current] == 0) {
+                            visited[current] = 1;
+                            parent[current] = vertex;
+                            stack.add(current);
+                        } else if(parent[vertex] != current){
+                            return false;
+                        }
+                   }
+                }
+        return true;
     }
 
+    public boolean isValidTree_recursion_approach(int source,LinkedList<Integer>[] adjList){
 
-
-    public static void main(String[] args){
-
-        GraphTraversal gt = new GraphTraversal();
-
-        int[][] input = new int[][]{
-                {0,1},{0,3},
-                {1,0},{1,2},{1,3},{1,4},
-                {2,1},{2,3},{2,4},{2,5},
-                {3,0},{3,1},{3,2},{3,4},
-                {4,1},{4,2},{4,3},{4,5},
-                {5,2},{5,4}
-        };
-
-        Graph graph = new Graph(6);
-                graph.construct(input,true);
-
-        gt.hasComponents(graph.adjList);
-        System.out.println("is Eulerian cycle :: "+gt.hasEulerianCycle(graph.adjList));
-
-
+        visited = new int[adjList.length];
+        parent = new int[adjList.length];
+         this.adjList = adjList;
+        return isCycleDFS(source);
     }
 
+    public boolean isCycleDFS(int source){
+
+        visited[source] = 1;
+        for (int i=0;i<adjList[source].size();i++){
+              int currEdge = adjList[source].get(i);
+            if(visited[currEdge] == 0){
+                parent[currEdge] = source;
+                if(isCycleDFS(currEdge)){
+                    return true;
+                }
+             } else {
+                if(parent[source] != currEdge){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 
 }
--- src/test/java/org/graphs/GraphTraversalTest.java
+++ src/test/java/org/graphs/GraphTraversalTest.java
@@ -6,13 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class GraphTraversalTest {
 
     @Test
-    void hasEulerianCycle() {
-
-
-
-    }
-
-    @Test
     void hasComponents() {
 
         GraphTraversal gt = new GraphTraversal();
@@ -31,6 +24,28 @@ class GraphTraversalTest {
         graph.construct(input,true);
 
         assertEquals(1, gt.hasComponents(graph.adjList));
+
+    }
+
+    @Test
+    void hasEulerian() {
+
+        GraphTraversal gt = new GraphTraversal();
+
+        int[][] input = new int[][]{
+                {0,1},{0,3},
+                {1,0},{1,2},{1,3},{1,4},
+                {2,1},{2,3},{2,4},
+                {3,0},{3,1},{3,2},{3,4},
+                {4,1},{4,2},{4,3},
+                {5,6},
+                {6,5}
+        };
+
+        Graph graph = new Graph(7);
+        graph.construct(input,true);
+
+        assertTrue(gt.hasEulerianCycle(graph.adjList));
 
     }
 
@@ -71,8 +86,107 @@ class GraphTraversalTest {
 
     }
 
+    @Test
+    void isValidTree() {
+
+        GraphTraversal gt = new GraphTraversal();
+
+        int[][] input = new int[][]{
+                {0,1},{1,2}
+        };
+
+        Graph graph = new Graph(3);
+        graph.construct(input,true);
+
+        assertTrue(gt.isValidTree_bfs_approach(0, graph.adjList));
+
+    }
+    @Test
+    void isValidTree1() {
+
+        GraphTraversal gt = new GraphTraversal();
+
+        int[][] input = new int[][]{
+                {0,1},
+                {0,2},
+                {1,2}
+        };
+
+        Graph graph = new Graph(3);
+        graph.construct(input,true);
+
+        assertFalse(gt.isValidTree_bfs_approach(0, graph.adjList));
+
+    }
 
     @Test
-    void bfs() {
+    void isValidTree2() {
+
+        GraphTraversal gt = new GraphTraversal();
+
+        int[][] input = new int[][]{
+                {0,1},
+                {0,2}
+        };
+
+        Graph graph = new Graph(3);
+        graph.construct(input,true);
+
+        assertTrue(gt.isValidTree_dfs_stack_approach(0, graph.adjList));
+
     }
+
+    @Test
+    void isValidTree3() {
+
+        GraphTraversal gt = new GraphTraversal();
+
+        int[][] input = new int[][]{
+                {0,1},
+                {0,2},
+                {1,2}
+        };
+
+        Graph graph = new Graph(3);
+        graph.construct(input,true);
+
+        assertFalse(gt.isValidTree_dfs_stack_approach(0, graph.adjList));
+
+    }
+
+    @Test
+    void isValidTree4() {
+
+        GraphTraversal gt = new GraphTraversal();
+
+        int[][] input = new int[][]{
+                {0,1},
+                {0,2},
+                {1,2}
+        };
+
+        Graph graph = new Graph(3);
+        graph.construct(input,true);
+
+        assertTrue(gt.isValidTree_recursion_approach(0, graph.adjList));
+
+    }
+
+    @Test
+    void isValidTree5() {
+
+        GraphTraversal gt = new GraphTraversal();
+
+        int[][] input = new int[][]{
+                {0,1},
+                {0,2}
+        };
+
+        Graph graph = new Graph(3);
+        graph.construct(input,true);
+
+        assertFalse(gt.isValidTree_recursion_approach(0, graph.adjList));
+
+    }
+
 }
